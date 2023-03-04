24a49ec02c47671e70208201bd13ad8312e9a0b4
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-03-04 20:58:04 +0100

    UnitTests for reserved keywords.

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,8 @@ pub mod parser;
 fn main() {
     println!("Hello, world!");
 
-    let symbol = PythonCoreTokenizer::is_keyword("False", 11, 16);
+    let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+    let symbol = lexer.is_keyword("False", 11, 16);
     match symbol {
         Some( _ ) => println!("Found:"),
         _ => println!("Empty!")
--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -11,8 +11,8 @@ pub struct PythonCoreTokenizer {
 
 pub trait Tokenizer {
     fn new() -> PythonCoreTokenizer;
-    fn tokenize(buffer: &str) -> Result<Box<Vec<Box<TokenSymbol>>>, String>;
-    fn is_keyword(text: &str, start: u32, end: u32) -> Option<TokenSymbol>;
+    fn tokenize(&self, buffer: &str) -> Result<Box<Vec<Box<TokenSymbol>>>, String>;
+    fn is_keyword(&self, text: &str, start: u32, end: u32) -> Option<TokenSymbol>;
 }
 
 
@@ -25,12 +25,12 @@ impl Tokenizer for PythonCoreTokenizer {
         }
     }
 
-    fn tokenize(buffer: &str) -> Result<Box<Vec<Box<TokenSymbol>>>, String> {
+    fn tokenize(&self, buffer: &str) -> Result<Box<Vec<Box<TokenSymbol>>>, String> {
         let a = Box::new(Vec::from( [ Box::new(TokenSymbol::PyEof) ] ));
         Ok(a)
     }
 
-    fn is_keyword(text: &str, start_pos: u32, end_pos: u32) -> Option<TokenSymbol> {
+    fn is_keyword(&self, text: &str, start_pos: u32, end_pos: u32) -> Option<TokenSymbol> {
         match text {
             "False" => Some(TokenSymbol::PyFalse(start_pos, end_pos)),
             "None" => Some(TokenSymbol::PyNone(start_pos, end_pos)),
@@ -71,5 +71,475 @@ impl Tokenizer for PythonCoreTokenizer {
         }
     }
 
+}
 
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn reserved_keyword_false() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("False", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyFalse(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_none() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("None", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyNone(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_true() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("True", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyTrue(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_and() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("and", 1, 4);
+        match symbol {
+            Some(TokenSymbol::PyAnd(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 4)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_as() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("as", 1, 3);
+        match symbol {
+            Some(TokenSymbol::PyAs(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 3)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_assert() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("assert", 1, 7);
+        match symbol {
+            Some(TokenSymbol::PyAssert(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 7)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_async() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("async", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyAsync(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_await() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("await", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyAwait(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_break() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("break", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyBreak(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_class() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("class", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyClass(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_continue() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("continue", 1, 9);
+        match symbol {
+            Some(TokenSymbol::PyContinue(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 9)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_def() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("def", 1, 4);
+        match symbol {
+            Some(TokenSymbol::PyDef(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 4)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_del() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("del", 1, 4);
+        match symbol {
+            Some(TokenSymbol::PyDel(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 4)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_elif() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("elif", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyElif(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_else() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("else", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyElse(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_except() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("except", 1, 7);
+        match symbol {
+            Some(TokenSymbol::PyExcept(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 7)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_finally() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("finally", 1, 8);
+        match symbol {
+            Some(TokenSymbol::PyFinally(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 8)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_for() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("for", 1, 4);
+        match symbol {
+            Some(TokenSymbol::PyFor(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 4)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_from() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("from", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyFrom(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_global() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("global", 1, 7);
+        match symbol {
+            Some(TokenSymbol::PyGlobal(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 7)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_if() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("if", 1, 3);
+        match symbol {
+            Some(TokenSymbol::PyIf(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 3)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_import() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("import", 1, 7);
+        match symbol {
+            Some(TokenSymbol::PyImport(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 7)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_in() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("in", 1, 3);
+        match symbol {
+            Some(TokenSymbol::PyIn(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 3)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_is() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("is", 1, 3);
+        match symbol {
+            Some(TokenSymbol::PyIs(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 3)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_lambda() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("lambda", 1, 7);
+        match symbol {
+            Some(TokenSymbol::PyLambda(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 7)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_nonlocal() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("nonlocal", 1, 9);
+        match symbol {
+            Some(TokenSymbol::PyNonlocal(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 9)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_not() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("not", 1, 4);
+        match symbol {
+            Some(TokenSymbol::PyNot(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 4)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_or() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("or", 1, 3);
+        match symbol {
+            Some(TokenSymbol::PyOr(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 3)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_pass() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("pass", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyPass(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_raise() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("raise", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyRaise(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_return() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("return", 1, 7);
+        match symbol {
+            Some(TokenSymbol::PyReturn(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 7)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_try() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("try", 1, 4);
+        match symbol {
+            Some(TokenSymbol::PyTry(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 4)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_while() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("while", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyWhile(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_with() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("with", 1, 5);
+        match symbol {
+            Some(TokenSymbol::PyWith(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 5)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_yield() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("yield", 1, 6);
+        match symbol {
+            Some(TokenSymbol::PyYield(s, e)) => {
+                assert_eq!(s, 1);
+                assert_eq!(e, 6)
+            },
+            _ => assert!(false)
+        }
+    }
+
+    #[test]
+    fn reserved_keyword_not_a_keyword() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new();
+        let symbol = lexer.is_keyword("__init__", 1, 9);
+        match symbol {
+            None => assert!(true),
+            _ => assert!(false)
+        }
+    }
 }
