bddf894720c2d0efe80a19e82b5a76cce0b83257
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-03-05 16:35:12 +0100

    Added UnitTest for floor div assign.

--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -642,4 +642,14 @@ mod tests {
             _ => assert!(false)
         }
     }
+
+    #[test]
+    fn operator_or_delimiter_floor_assign() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new(String::from("Unused!"), 4);
+        let symbol = lexer.is_operator_or_delimiter('/', '/', '=', 1);
+        match symbol {
+            Some( ( TokenSymbol::PyFloorDivAssign(1, 4), 3 ) ) => assert!(true),
+            _ => assert!(false)
+        }
+    }
 }
