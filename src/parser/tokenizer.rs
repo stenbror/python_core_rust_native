25a472cf76d09668a8ce2362063243d8ea3310be
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-03-05 17:23:03 +0100

    Added UnitTest for greater equal.

--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -742,4 +742,14 @@ mod tests {
             _ => assert!(false)
         }
     }
+
+    #[test]
+    fn operator_or_delimiter_greater_equal() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new(String::from("Unused!"), 4);
+        let symbol = lexer.is_operator_or_delimiter('>', '=', ' ', 1);
+        match symbol {
+            Some( ( TokenSymbol::PyGreaterEqual(1, 3), 2 ) ) => assert!(true),
+            _ => assert!(false)
+        }
+    }
 }
