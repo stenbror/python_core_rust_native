cf05c5d13d9aa5c245d393e1ca82c7a0d2201a2f
Richard Magnor Stenbro <stenbror@hotmail.com>
2023-03-05 17:06:36 +0100

    Added UnitTest for shift right assign.

--- src/parser/tokenizer.rs
+++ src/parser/tokenizer.rs
@@ -722,4 +722,14 @@ mod tests {
             _ => assert!(false)
         }
     }
+
+    #[test]
+    fn operator_or_delimiter_shift_right_assign() {
+        let lexer : PythonCoreTokenizer = PythonCoreTokenizer::new(String::from("Unused!"), 4);
+        let symbol = lexer.is_operator_or_delimiter('>', '>', '=', 1);
+        match symbol {
+            Some( ( TokenSymbol::PyShiftRightAssign(1, 4), 3 ) ) => assert!(true),
+            _ => assert!(false)
+        }
+    }
 }
